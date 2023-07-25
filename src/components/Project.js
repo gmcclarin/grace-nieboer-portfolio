814bece82a65139fe8a7a5189c84961de65ffc02
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-25 09:59:56 -0400

    add tailwind cards for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,23 @@
 export default function Project({p}) {
     return (
-        <div className="">
-            <div>{p.name}</div>
-            <div>{p.descript}</div>
-            <a href={p.github}>github</a>
-            <a href={p.demo}>demo</a>
-        </div>
+            <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+                <img
+                    className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                    src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+                    alt="" />
+                <div className="flex flex-col justify-start p-6">
+                    <h5
+                    className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                    {p.name}
+                    </h5>
+                    <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                    {p.descript}
+                    </p>
+                    <p className="text-xs text-neutral-500 dark:text-neutral-300">
+                    <a href={p.github}>github</a>
+                    </p>
+                    <a href={p.demo}>demo</a>
+                    </div>
+                </div>
     )
 }
