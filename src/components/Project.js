e29fc744ef649a67138c1702e52c9afab6477271
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-08 17:14:41 -0400

    image of first project came throughgit add .git add .

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ export default function Project({p}) {
             <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
                 <img
                     className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                    src="{{ url_for('static', filename={p.photo.name}}) }}" alt={p.name}
+                    src={p.photos[0].url} alt={p.name}
                     />
                 <div className="flex flex-col justify-start p-6">
                     <h5
