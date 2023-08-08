74ecdc4abce37d7d1284482117741e3f32adb715
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-07 22:44:10 -0400

    mobile styling 'about' component

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,11 @@ import tongue from './tongue.png'
 function About () {
 
     return (
-        <div className="p-16 snap-always snap-center">
+        <div className="sm:p-16 snap-always snap-center">
         <div className="mx-8  mb-8">
-            <p id="about" className="text-5xl sm:text-7xl font-display sm:p-10 my-7 sm:my-6">HI THERE, AND WELCOME! </p>
-            <div className=" mb-5 pb-10 flex ">
-                <p className = "text-xl sm:text-4xl text-justify sm:w-1/2 ">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with building API's with REST-ful architecture. I am currently learning Java and collaborating on a few projects with other teams. In my free time, you'll find me kickboxing or eating pizza</p>
+            <p id="about" className="text-5xl sm:text-7xl font-display pb-10 my-7 sm:my-6">HI THERE, AND WELCOME! </p>
+            <div className=" mb-5 sm:pb-10 flex ">
+                <p className = "text-xl sm:text-4xl text-justify sm:w-1/2 pb-10">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with building API's with REST-ful architecture. I am currently learning Java and collaborating on a few projects with other teams. In my free time, you'll find me kickboxing or eating pizza</p>
                 {/* <img alt="tongue" className="h-2/5 " src={tongue} /> */}
             </div>
             <div className="mb-5 pb-10 flex justify-end ">
