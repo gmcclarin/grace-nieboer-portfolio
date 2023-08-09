0fe3d668db3fb5453bb7b86bffb583fe2d0356b8
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-08 22:12:39 -0400

    NotFound styling and button added that goes back one page using useHistory hook

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,25 @@
+import { useState } from "react"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
 export default function NotFound () {
+    const history= useHistory()
+    const goBackOne = () => history.goBack()
+
+
     return (
-        <>
-        404 not found
-        </>
+        <div className="">
+            <div className=" mt-20 p-5 text-5xl font-display font-bold text-center">
+                Whoops! Page Not Found. 
+            </div>
+            <div className=" p-5 text-5xl font-display font-bold text-center">
+                Are you lost? 
+            </div>
+            <div className="text-center">
+                <button className=" hover:bg-gray-300 bg-orange-600 text-white border-2 text-xl sm:text-2xl border-white font-bold py-2 px-4 sm:py-4 sm:px-8 rounded-full mx-8 my-8"
+                onClick={goBackOne}>
+                    GO BACK
+                </button>
+            </div>
+        </div>
     )
 }
