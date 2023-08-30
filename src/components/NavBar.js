a8ae13488eb883162abbd42219dd50fe522af8d9
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-30 11:03:51 -0400

    portfolio display styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,63 +3,57 @@ import { NavLink } from "react-router-dom";
 
 function NavBar () {
     return (
-        <nav className="bg-transparent ">
-            <div className="px-2 my-5 ">
+        <nav className="bg-transparent">
+            <div className="my-5">
                 <div className="flex">
-                    <div className="invisible sm:visible sm:w-1/3 sm:flex-col sm:justify-items-start ">
+                    <div className="invisible sm:visible sm:w-1/4 sm:flex-col sm:justify-items-start ">
                         <div>
                             <NavLink
                             to="/"
                             exact="true"
-                            className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
+                            className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium"
                             >
                                 HOME
                             </NavLink>
                             <NavLink
                             to="/about"
                             exact="true"
-                            className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
+                            className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium"
                             >
                                 ABOUT
                             </NavLink>
                             <NavLink
                             to="/portfolio"
                             exact="true"
-                            className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
+                            className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium"
                             >
                                 WORK
                             </NavLink>
                             <NavLink
                             to="/contact"
                             exact="true"
-                            className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium"
+                            className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium"
                             >CONTACT
                             </NavLink>
                         </div>
                         <div>
-                        <a className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium" 
+                        <a className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium" 
                                 href="mailto:grace.nieboer@outlook.com"
                                 >EMAIL  
                             </a>
-                            <a className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium" 
-                                href="https://www.instagram.com/grayce.elisabeth_/"
-                                target="_blank" 
-                                rel="noreferrer"
-                                >INSTAGRAM 
-                            </a>
-                            <a className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium" 
+                            <a className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium" 
                                 href="https://medium.com/@grace.nieboer"
                                 target="_blank"
                                 rel="noreferrer" 
                                 >BLOG
                             </a>
-                            <a className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium" 
+                            <a className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium" 
                                 href="https://github.com/gnieb"
                                 target="_blank" 
                                 rel="noreferrer"
                                 >GITHUB
                             </a>
-                            <a className="hover:text-white rounded-md px-3 py-2 text-sm sm:text-xl font-medium" 
+                            <a className="hover:text-white rounded-md px-2 py-2 text-sm sm:text-xl font-medium" 
                                 href="https://www.linkedin.com/in/gracenieboer/" 
                                 target="_blank" 
                                 rel="noreferrer"
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,20 +5,22 @@ export default function ({p}) {
     
 
     return (
-        <div className="mx-5 mb-10">
+        <div className=" p-10 mb-40 bg-green-900 bg-opacity-50">
             <div className="font-fun font-bold p-4">
-                <div className="text-4xl sm:text-6xl underline">{p.name.toUpperCase()}</div>
+                <div className="text-4xl sm:text-6xl ">{p.name.toUpperCase()}</div>
             </div>
-            <div className="flex justify-end m-2 ">
-                <div className="w-1/2 m-3 text-xl">{p.descript}
-                <div className="m-4">
+            <div className="m-2 text-right">
+                <div className="m-3">
+                    <div className="font-bold sm:text-2xl">{p.descript}</div>
+                    <div className="m-4">
                         <div className="text-2xl underline font-bold">Built With</div>
                         <div className="text-2xl">{p.front_end}</div>
                         <div className="text-2xl">{p.back_end}</div>
                         <div className="text-2xl">{p.database}</div>
                         <div className="text-2xl">{p.css}</div>
-                </div></div>
-                <div className="w-full sm:w-3/4 mb-10" >
+                    </div>
+                </div>
+                <div className="w-full mb-10" >
                     
                         <ResponsiveMasonry
                             columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
