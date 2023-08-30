bdcf33871d31e48eca2a3bee47c715ca12bbb25d
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-30 12:28:13 -0400

    create photo folder to hold photos, and add portfoli pictures for About component. import responsive masonry to display photos

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
-import tongue from './tongue.png'
+import lookatcamera from '../photos/lookatcameraportfolio3.png'
+import lookdown from '../photos/lookingdownportfolio1.png'
+import portfolio4 from '../photos/portfolio4.jpg'
+import portoflio5 from '../photos/portfolio5.png'
+import glenedith from '../photos/glenedith.png'
+import starbs from '../photos/starbsandcaution.png'
 import NavBar from './NavBar';
 import MobileMenu from './MobileMenu';
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
 function About () {
+    const photos = [lookatcamera, starbs, portoflio5, lookdown, portfolio4 ]
+
     return (
         <div className="m-5 ">
             <MobileMenu />
@@ -10,8 +18,25 @@ function About () {
             <p className="text-5xl sm:text-[200px] font-fun font-bold pb-10 my-7 sm:my-6">about.</p>
             <div className=" mb-5 sm:pb-10 flex ">
                 <p className = "text-xl sm:text-4xl text-justify sm:w-1/2 pb-10">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with building API's with REST-ful architecture. I am currently learning Java and collaborating on a few projects with other teams. In my free time, you'll find me kickboxing or eating pizza</p>
-                
-                    <img alt="tongue" className="pl-10 w-1/3 h-1/3" src={tongue} />
+                <div className="w-1/2 m-5 mb-10" >
+                    
+                        <ResponsiveMasonry
+                            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                            >
+                            <Masonry gutter="20px" >
+                                {photos.map((photo, i) => {
+                                        return (
+                                        <img
+                                            key={i}
+                                            src={photo}
+                                            style={{width: "100%", display: "block"}}
+                                            alt="portfolio picture"
+                                        /> 
+                                        )
+                             })}
+                            </Masonry>
+                        </ResponsiveMasonry>
+                    </div>
                 
             </div>
             <div className="mb-5 pb-10 flex justify-end ">
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,10 @@ function Home () {
             <div className="sm:flex sm:justify-between">
                 <div className="flex-col w-1/4">
                     <div className="font-bold pb-5 text-xl sm:text-3xl">Grace Nieboer</div>
-                    <div className="text-xxl sm:text-2xl lg:text-3xl pb-5">SOFTWARE ENGINEER. DEVELOPER. ARCHITECT.</div>
-                    <div className="text-xxl sm:text-2xl lg:text-3xl">PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER.</div>
+                    <div className="text-xxl sm:text-2xl lg:text-2xl xl:text-3xl pb-5">SOFTWARE ENGINEER. DEVELOPER. ARCHITECT.</div>
+                    <div className="text-xxl sm:text-2xl lg:text-2xl xl:text-3xl">PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER.</div>
                 </div>
-                <div className = "text-8xl sm:pt-0 pt-20 md:text-[150px] xl:text-[250px] 2xl:text-[300px] text-right font-fun font-black m-2 ">
+                <div className = "text-8xl sm:pt-0 pt-20 md:text-[150px] xl:text-[220px] 2xl:text-[320px] text-right font-fun font-black m-2 ">
                     <div className = "hover:italic">
                     <NavLink exact="true" to='/about'>about.</NavLink> 
                     </div>
