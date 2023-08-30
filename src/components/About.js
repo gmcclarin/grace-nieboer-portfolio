918ca2166816e52d25b69c0a1110498b892cd333
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-30 13:01:30 -0400

    style mobile friendly responsive css for about page

--- src/components/About.js
+++ src/components/About.js
@@ -15,13 +15,13 @@ function About () {
         <div className="m-5 ">
             <MobileMenu />
         <div className="mx-8  mb-8">
-            <p className="text-5xl sm:text-[200px] font-fun font-bold pb-10 my-7 sm:my-6">about.</p>
-            <div className=" mb-5 flex ">
-                <p className = "text-xl sm:text-4xl text-justify sm:w-1/2 pb-10">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with building API's with REST-ful architecture. I am currently learning Java and collaborating on a few projects with other teams. In my free time, you'll find me kickboxing or eating pizza</p>
-                <div className="w-1/2 m-5 mb-10" >
+            <p className="text-6xl sm:text-[200px] font-fun font-bold pb-10 my-7 sm:my-6">about.</p>
+            <div className=" mb-5 sm:flex ">
+                <p className = "text-xl sm:text-2xl lg:text-3xl xl:text-4xl text-justify w-full sm:w-1/2 pb-10">My name is Grace Nieboer. I am a full stack software engineer with a love for the creative elements of front-end design, but I recently fell in love with Python on the back-end along with building API's with REST-ful architecture. I am currently learning Java and collaborating on a few projects with other teams. In my free time, you'll find me kickboxing or eating pizza</p>
+                <div className="w-full sm:w-1/2 m-5 mb-10" >
                     
                         <ResponsiveMasonry
-                            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                            columnsCountBreakPoints={{350: 2, 900: 3}}
                             >
                             <Masonry gutter="20px" >
                                 {photos.map((photo, i) => {
@@ -42,10 +42,10 @@ function About () {
             <div className="mb-5 pb-10 flex justify-end ">
                 <div className="sm:w-1/2 flex-col text-right">
                 <p className="text-3xl font-bold">BACKGROUND</p>
-                <p className="text-xl sm:text-4xl text-justify pb-10">I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to work through employee issues and resolve conflicts. 
+                <p className="text-xl sm:text-2xl lg:text-4xl text-justify pb-10">I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to work through employee issues and resolve conflicts. 
                 </p>
                 <p className="text-3xl font-bold mt-3">PERSON-FOCUSED</p>
-                <p className="text-xl sm:text-4xl text-justify">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work WITH people, FOR people.</p>
+                <p className="text-xl sm:text-2xl lg:text-4xl text-justify">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work WITH people, FOR people.</p>
                 </div>
              </div>
         </div>
