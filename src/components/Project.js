175d905307d50953c4419faa180f8a2b80c2034c
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-02 11:59:50 -0400

    add bullets to project display with flexbox styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -20,6 +20,11 @@ export default function ({p}) {
                         <div className="text-3xl">{p.css}</div>
                     </div>
                 </div>
+                <div className="flex justify-between">
+                    <div>{p.bullet1}</div>
+                    <div>{p.bullet2}</div>
+                </div>
+                <div>{p.bullet3}</div>
                 <div className="w-full mb-10" >
                     
                         <ResponsiveMasonry
