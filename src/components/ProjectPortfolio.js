38a8f7771e84e31d2018b7c38a303bbbb069b4fa
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-07 23:28:58 -0400

    portfolio nav created as a possibility but not necessary

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -20,48 +20,41 @@ function ProjectPortfolio () {
                     {/* <div className="flex justify-center">
                         <h2 className="text-9xl sm:text-[150px] font-fun pb-10 font-bold">work.</h2>
                     </div> */}
-
-                    <nav className="text-sm sm:text-2xl font-fun">
-                        <NavLink
-                            to="/portfolio/inspiredinteriors"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md p-5  font-bold -rotate-90"
-                            >INSPIRED INTERIORS
-                        </NavLink>
-                        <NavLink
-                            to="/portfolio/jobsurge"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
-                            >JOBSURGE
-                        </NavLink>
-                        <NavLink
-                            to="/portfolio/twoormore"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
-                            >TWO OR MORE
-                        </NavLink>
-                        <NavLink
-                            to="/portfolio/fryme"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
-                            >FRY ME TO THE MOON
-                        </NavLink>
-
-                    </nav>
                     <div className="sm:w-11/12 mx-5 flex-col text-right">
                     </div>
                     <div className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll">
                         <div className="snap-start snap-always bg-pink-500 w-screen h-screen flex items-center justify-center ">
-                        <NavLink
-                            to="/inspiredinteriors"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md p-5  font-bold -rotate-90"
-                            >INSPIRED INTERIORS
-                        </NavLink>
+                            <NavLink
+                                to="/inspiredinteriors"
+                                exact="true"
+                                className="hover:text-orange-500 rounded-md p-5  font-bold -rotate-90"
+                                >INSPIRED INTERIORS
+                            </NavLink>
                         </div>
-                        <div className="snap-start snap-always bg-blue-500 w-screen h-screen flex items-center justify-center ">1</div>
-                        <div className="snap-start snap-always bg-orange-500 w-screen h-screen flex items-center justify-center ">1</div>
-                        <div className="snap-start snap-always bg-teal-500 w-screen h-screen flex items-center justify-center ">1</div>
+                        <div className="snap-start snap-always bg-blue-500 w-screen h-screen flex items-center justify-center ">
+                            <NavLink
+                                to="/twoormore"
+                                exact="true"
+                                className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
+                                >TWO OR MORE
+                            </NavLink>
+                        </div>
+                        <div className="snap-start snap-always bg-orange-500 w-screen h-screen flex items-center justify-center ">
+                            <NavLink
+                                to="/jobsurge"
+                                exact="true"
+                                className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
+                                >JOBSURGE
+                            </NavLink>
+                        </div>
+                        <div className="snap-start snap-always bg-teal-500 w-screen h-screen flex items-center justify-center ">
+                            <NavLink
+                                to="/fryme"
+                                exact="true"
+                                className="hover:text-orange-500 rounded-md p-5 font-bold -rotate-90"
+                                >FRY ME TO THE MOON
+                            </NavLink>
+                        </div>
                         
                     </div>
                 </div>
