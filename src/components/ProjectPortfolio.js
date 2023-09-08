020f539d88bcabcb964fea5fe5276d643e363dda
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-08 13:44:31 -0400

    add Time component

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -1,19 +1,17 @@
 import NavBar from "./NavBar";
 import MobileMenu from "./MobileMenu";
 import { NavLink } from "react-router-dom";
-import photo20 from "../photos/photo20.avif";
 
 function ProjectPortfolio () {
  
     
     return (
-        <div className="relative block">
+        <div className="relative block overscroll-contain">
         <div className="sm:hidden"><MobileMenu /></div>
         <NavBar />
         <div className="flex">
                 <div className = "mx-auto text-center">
-                    {/* <img src={photo20} /> */}
-                    <div className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll text-5xl">
+                    <div className="snap-y snap-mandatory h-screen w-screen  overflow-y-scroll overflow-x-hidden text-6xl">
                         <div id="portsection1" className="snap-start snap-always w-screen h-screen flex items-center justify-center ">
                             <NavLink
                                 to="/inspiredinteriors"
@@ -30,11 +28,23 @@ function ProjectPortfolio () {
                                 >TWO OR MORE
                             </NavLink>
                         </div>
-                        <div className="snap-start snap-always bg-zinc-500 w-screen h-screen flex items-center justify-center ">
+                        <div id="portsection3" className="relative snap-start snap-always bg-zinc-500 w-screen h-screen mx-auto ">
+                        <video
+                            autoPlay
+                            loop
+                            muted
+                            className="w-screen h-screen"
+                        >
+                                <source
+                                src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+                                type="video/mp4"
+                                />
+                            Your browser does not support the video tag.
+                            </video>
                             <NavLink
                                 to="/jobsurge"
                                 exact="true"
-                                className="hover:text-orange-500 rounded-md p-5 font-bold"
+                                className="text-white absolute right-0 bottom-0 mx-auto hover:text-orange-500 rounded-md p-5 font-bold"
                                 >JOBSURGE
                             </NavLink>
                         </div>
