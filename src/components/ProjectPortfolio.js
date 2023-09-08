4a1c3112bff88c91ff5007e68433dc260021fd7a
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-07 20:44:28 -0400

    add a mini nav bar on WORK with one nav link. add nested route for work portfolio componenet

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -5,28 +5,34 @@ import MobileMenu from "./MobileMenu";
 import Loading from "./Loading"
 import InspiredInteriors from "./InspiredInteriors";
 import {Routes, Route} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 function ProjectPortfolio () {
  
-
     
     return (
         <div className="m-1 sm:m-5 relative block">
         <div className="sm:hidden"><MobileMenu /></div>
         <NavBar />
         <div className="m-5 flex">
-                <div className = "flex-col justify-center text-center">
+                <div className = "mx-auto text-center">
                     <div className="flex justify-center">
                         <h2 className="text-9xl sm:text-[200px] font-fun pb-10 font-bold">work.</h2>
                     </div>
+                    <nav>
+                        <NavLink
+                            to="/portfolio/inspiredinteriors"
+                            exact="true"
+                            className="hover:text-orange-500 rounded-md py-20  text-sm sm:text-xl font-bold -rotate-90"
+                            >INSPIRED INTERIORS
+                        </NavLink>
+                        
+                    </nav>
                     <div className="flex justify-end ">
                         <div className="sm:w-11/12 mx-5 flex-col text-right">
                         <Routes>
-                            <Route exact path="/" element={<Home />}/>
-                            <Route path="about" element={<About />} />
-                            <Route path="contact" element={<Contact />}/>
-                            <Route path="portfolio/*" element={<ProjectPortfolio />}/>
-                            <Route path="*" element={<NotFound />} />
+                            <Route path="/inspiredinteriors" element={<InspiredInteriors />}/>
+                            {/* <Route path="portfolio/*" element={<ProjectPortfolio />}/> */}
                         </Routes>
                         </div>
                     </div>
