04ab870436b87599455aa4ad8d2cdbe93469d1c6
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-08 22:52:45 -0400

    home squares interactive

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,20 +13,20 @@ function Home () {
                         <h1 className="absolute text-4xl sm:text-[100px] text-white top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">Grace Nieboer</h1>
                     </div>
                     <div className="flex ">
-                        <div className="relative w-full h-0 pb-1/2 text-sm sm:text-2xl lg:text-2xl xl:text-3xl border border-black">
-                            <p className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">SOFTWARE ENGINEER. DEVELOPER. ARCHITECT.</p>
+                        <div className="bg-neutral-100 relative w-full h-0 pb-1/2 text-sm sm:text-2xl lg:text-2xl xl:text-3xl border-2 border-black transform transition duration-500 hover:scale-125 hover:z-10">
+                            <p className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">SOFTWARE ENGINEER. DEVELOPER. ARCHITECT.</p>
                         </div>
-                        <div className="relative w-full h-0 pb-1/2 text-sm sm:text-2xl xl:text-2xl">
+                        <div className="relative w-full h-0 pb-1/2 text-sm sm:text-2xl xl:text-2xl border-2 border-black transform transition duration-500 hover:scale-125 bg-neutral-100 hover:z-10">
                             <p className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">PASSIONATE DEVELOPER WITH A LOVE FOR FINDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER.</p>
                         </div>
                     </div>
                 </div>
                 <div className = "w-full lg:w-1/2  h-screen text-white text-4xl sm:text-8xl text-center font-fun font-black">
                     <div className="flex ">
-                        <div id="home-about" className = "relative w-full h-0 pb-1/2 hover:italic grayscale hover:grayscale-0 bg-pink-300">
+                        <div id="home-about" className = "relative w-full h-0 pb-1/2 hover:italic grayscale hover:grayscale-0 transform transition duration-500 hover:scale-125 hover:z-10">
                             <NavLink className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2" exact="true" to='/about'>about.</NavLink> 
                         </div>
-                        <div id="home-work" className = "relative w-full h-0 pb-1/2 hover:italic grayscale hover:grayscale-0">
+                        <div id="home-work" className = "relative w-full h-0 pb-1/2 hover:italic grayscale hover:grayscale-0 transform transition duration-500 hover:scale-125 hover:z-10">
                             <video
                             id="video-element"
                                 autoPlay
