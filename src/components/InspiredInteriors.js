defd991b3d599ca7f30e52d2e19000271157155f
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-12 13:30:43 -0400

    css for explore feature of inspired interior portfolio highlight

--- src/components/InspiredInteriors.js
+++ src/components/InspiredInteriors.js
@@ -1,6 +1,7 @@
 
 import { NavLink } from "react-router-dom";
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
+import explore from '../photos/explorepage.png'
 
 export default function InspiredInteriors () {
 
@@ -23,15 +24,16 @@ export default function InspiredInteriors () {
     ]
 
     return (
-        <div className="relative overflow-y-hidden">
+        <div className="relative overflow-y-hidden overflow-x-hidden ">
+
             <NavLink
                 to="/portfolio"
                 exact="true"
-                className="absolute hover:text-orange-500 rounded-md p-10 py-15 text-sm sm:text-2xl font-bold "
+                className="fixed -rotate-90 hover:text-orange-500 text-sm sm:text-2xl font-bold translate-y-28 -translate-x-12 top-0 left-0"
                 >
-                    ← BACK TO WORK ←
+                    BACK TO WORK
             </NavLink>
-            <div className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll overflow-x-hidden">
+            <div className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll overflow-x-hidden ">
                 <div id="inspfirstsection" className="snap-start snap-always w-screen h-screen flex justify-center items-center">
                     <div className="text-4xl pb-5 sm:text-9xl font-bold transform translate-y-12 transition-opacity swishy2">INSPIRED INTERIORS</div>
                 </div>
@@ -40,12 +42,12 @@ export default function InspiredInteriors () {
                         <div className="text-4xl pb-5 sm:text-9xl">Inspired Interiors</div>
                         <div className="flex w-3/4 justify-between mx-auto ">
                             <div className="w-1/2 p-2 text-left file:font-bold sm:text-4xl italic text-black"> {`[An interior design planning and inspiration solution]`}</div>
-                            <div className="w-1/2 p-2 text-left font-bold sm:text-3xl italic text-black">This full stack application was built to explore trending design ideas and create moodboards with a visual tool to experiment with different elements of design together.</div>
+                            <div className="w-1/2 p-2 text-left font-bold sm:text-3xl italic text-black">This full stack application was built to explore trending design ideas and create moodboards as a visual tool to experiment with different elements of design together.</div>
                         </div>
                         
                     </div>
                 </div>
-                <div className="snap-start snap-always w-screen h-screen flex justify-center mx-auto pl-5 items-center">
+                <div className="snap-start snap-always w-screen h-screen flex justify-center mx-auto items-center">
                 <div className="m-4">
                         <div className="text-4xl  font-bold">THE TECH STACK</div>
                         <div className="text-3xl">REACT JS</div>
@@ -57,16 +59,21 @@ export default function InspiredInteriors () {
                         <div className="text-3xl">MATERIAL UI</div>
                     </div>
                 </div>
-                <div className="snap-start snap-always w-screen h-screen flex items-center ">
-                    <div className="">
-                        <div><img className="mx-auto content-center w-3/4 2xl:w-1/2 border border-black" src={photos[0].src} />
+                <div className="snap-start snap-always w-screen h-screen  ">
+                   
+                        <div className="md:flex w-screen pt-12 px-32 justify-between items-center">
+                            <div className="xl:text-8xl md:text-6xl text-4xl font-black sm:w-1/3 w-full">EXPLORE</div>
+                            <div className=" w-full md:w-1/3 my-8 text-xl 2xl:text-3xl text-justify font-bold">The EXPLORE feature was inspired by the popular explore pages on social media platforms like Instagram.
+                            </div>
                         </div>
                     
-                        <div className="mx-auto sm:ml-10 w-3/4 md:w-1/3 py-8 text-2xl 2xl:text-3xl text-justify font-bold">The dashboard shows a live clock feature at the top as a reminder and motivation for me to take advantage of every minute.
+                        <div className="sm:pl-32  w-3/4 md:w-1/2 p-5 text-lg 2xl:text-2xl text-justify font-bold">The Explore feature brings current trends and design inspiration to the user from an industry classic: Architectural Digest (AD). Users can browse the top trending articles from AD and click on an article to read it at its source.
                         </div>
-                        <div className="mx-auto sm:ml-10 w-3/4 md:w-1/2 py-8 text-2xl 2xl:text-3xl text-justify font-bold"> There are goals displayed by time priority, along with a rotating random piece of advice or encouragement I've collected from experienced software developers.
+                       
+                        <div className="sm:flex sm:justify-center ">
+                            <img className="w-full md:w-3/4 2xl:w-1/2 p-7" src={explore} title="explore" />
                         </div>
-                    </div>  
+                 
                 </div>
                 <div className="snap-start snap-always w-screen h-screen flex items-center ">
                 <div>
