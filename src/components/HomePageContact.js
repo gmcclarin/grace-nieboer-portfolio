1e5a2777e1e1e384c5f201cb85ce0581e79a2515
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-17 16:10:53 -0400

    modify the home page social media links, adjust the second about highlight text size to be smaller for medium screens

--- src/components/HomePageContact.js
+++ src/components/HomePageContact.js
@@ -9,20 +9,20 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export default function HomePageContact () {
     return (
             <div className="flex content-center text-center">
-                <a className="transform transition duration-500 hover:scale-150" href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} style={{height:"40px",color: "#ffffff", margin:"8px"}}/>
+                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} style={{height:"40px",color: "#ffffff"}}/>
                 </a>
-                <a className="transform transition duration-500 hover:scale-150" href="mailto:grace.nieboer@outlook.com">
-                    <FontAwesomeIcon icon={faEnvelope} style={{height:"40px", color:"#ffffff", margin:"8px"}}/>
+                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="mailto:grace.nieboer@outlook.com">
+                    <FontAwesomeIcon icon={faEnvelope} style={{height:"40px", color:"#ffffff", }}/>
                 </a>
-                <a className="transform transition duration-500 hover:scale-150" href="https://www.instagram.com/grayce.elisabeth_/">
-                    <FontAwesomeIcon icon={faInstagram} style={{height:"40px", color:"#ffffff", margin:"8px"}}/>
+                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://www.instagram.com/grayce.elisabeth_/">
+                    <FontAwesomeIcon icon={faInstagram} style={{height:"40px", color:"#ffffff", }}/>
                 </a>
-                <a className="transform transition duration-500 hover:scale-150" href="https://medium.com/@grace.nieboer">
-                    <FontAwesomeIcon icon={faMedium} style={{height:"40px",color: "#ffffff", margin:"8px"}}/>
+                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://medium.com/@grace.nieboer">
+                    <FontAwesomeIcon icon={faMedium} style={{height:"40px",color: "#ffffff", }}/>
                 </a>
-                <a className="transform transition duration-500 hover:scale-150" href="https://github.com/gnieb">
-                    <FontAwesomeIcon icon={faGithub} style={{height:"40px",color: "#ffffff", margin:"7px"}} />
+                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://github.com/gnieb">
+                    <FontAwesomeIcon icon={faGithub} style={{height:"40px",color: "#ffffff",}} />
                 </a>
             </div>
             
