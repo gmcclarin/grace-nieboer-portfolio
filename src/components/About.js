bd2bfb4aab488b529a2ca65e9566864592cd006b
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-17 14:43:03 -0400

    modify about and background language description

--- src/components/About.js
+++ src/components/About.js
@@ -54,11 +54,11 @@ function About () {
             
             <div className="relative sm:mx-20 lg:mx-32 mb-5 flex h-screen items-center justify-center overscroll-x-auto overscroll-y-hidden">
 
-                {openAbout ? <div className = "aboutabout z-10 text-white  top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center px-8 rounded-full ">
+                {openAbout ? <div className = "aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center px-8 rounded-full ">
                     <div>
-                        <p className="p-1 py-2">HI, I'M GRACE! </p>
-                        <p className="p-1"> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
-                       <p className="p-1">I love Python as a language, along with building API's with REST-ful architecture.
+                        <p className="my-4 p-2 text-white bg-gray-900 opacity-50">HI, I'M GRACE! </p>
+                        <p className="my-4 p-2 text-white bg-gray-900 opacity-50"> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
+                       <p className="my-4 p-2 text-white bg-gray-900 opacity-50">I love Python as a language, along with building API's with REST-ful architecture.
                         </p>
                     </div>
                     </div> 
@@ -68,11 +68,11 @@ function About () {
                     </div> 
                     
                     }
-                { openBefore ? <div className = "aboutbackground z-10 text-white bg-neutral-500 top-50 left-50 text-xl sm:text-2xl  text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center px-8 rounded-full ">
+                { openBefore ? <div className = "aboutbackground z-10 text-white font-bold  top-50 left-50 text-xl sm:text-2xl  text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center px-8 rounded-full ">
                     <div>
-                        <p className="p-1">
-                        I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career working in employee relations roles within HR, partnering with multi-lingual, cross-functional teams to resolve employee conflicts. </p>
-                        <p className="p-1">My unique background in complex problem solving in a very person-focused industry and roles allows me better collaborate and communicate with diverse teams. We work in tech, but we work WITH people, FOR people.</p>
+                        <p className="my-4 p-2 text-white bg-gray-900 opacity-50">
+                        I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career partnering with multi-lingual, cross-functional teams to resolve conflicts. </p>
+                        <p className="my-4 p-2 text-white bg-gray-900 opacity-50">My unique professional background in complex problem-solving in person-focused roles allows me to better collaborate and communicate with diverse teams. I recognize that technology is built WITH people, and FOR people.</p>
                         </div>
                     </div> 
                 
@@ -81,11 +81,11 @@ function About () {
                 </div>}
 
                 {openNow ? 
-                <div className = "aboutnow z-10 text-white bg-neutral-600 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center p-8 rounded-full ">
-                    <div>
-                    <p className="p-1">I am currently learning Java, React Native, and Typescript</p> 
-                    <p className="p-1">I am currently learning Java, React Native, and Typescript</p>
-                    <p className="p-1"> In my free time, I'm probably kickboxing or eating pizza</p>
+                <div className = "aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center p-8 rounded-full ">
+                    <div className="">
+                    <p className="my-4 p-2 text-white bg-gray-900 opacity-50">I am currently learning Java, React Native, and Typescript.</p> 
+                    <p className="my-4 p-2 text-white bg-gray-900 opacity-50">I am collaborating on several projects with different teams to build quality, creative products.</p>
+                    <p className="my-4 p-2 text-white bg-gray-900 opacity-50"> In my free time, I'm probably kickboxing or thrifting somewhere.</p>
                     </div>
                 </div> 
                 
