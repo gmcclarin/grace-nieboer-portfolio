999ed480d0887c8e22b71dfec01cc1b35ea5b142
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-19 14:17:01 -0400

    rerouting resolved with _redirects and now testing overscroll on portfolio page so that url bar goes down on scroll..

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -59,7 +59,7 @@ function ProjectPortfolio () {
  
     
     return (
-        <div className="bg-white relative block overscroll-contain">
+        <div className="bg-white relative block ">
         <div className="sm:hidden"><MobileMenu /></div>
         <NavBar />
         <div className="flex">
