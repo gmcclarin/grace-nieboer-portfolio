b01177633977b44ba455ace1d2e959872cc55cd2
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-20 00:56:36 -0400

    resize about highlights and un0justify several text blocks

--- src/components/About.js
+++ src/components/About.js
@@ -48,11 +48,11 @@ function About () {
             
             <div className="relative sm:mx-20 lg:mx-32 mb-5 flex bg-neutral-100 h-screen items-center justify-center overscroll-x-auto overscroll-y-hidden">
 
-                {openAbout ? <div className = "aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:px-8 rounded-full ">
+                {openAbout ? <div className = "aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl lg:text-2xl  w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:px-8 rounded-full ">
                     <div>
-                        <p className="m-4 p-2 text-white bg-gray-900 opacity-50">HI, I'M GRACE! </p>
-                        <p className="m-4 p-2 text-white bg-gray-900 opacity-50"> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
-                       <p className="m-4 p-2 text-white bg-gray-900 opacity-50">I love Python as a language, along with building API's with REST-ful architecture.
+                        <p className="m-4 p-2 text-white bg-gray-900 opacity-70">HI, I'M GRACE! </p>
+                        <p className="m-4 p-2 text-white bg-gray-900 opacity-70"> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
+                       <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I love Python as a language, along with building API's with REST-ful architecture.
                         </p>
                     </div>
                     </div> 
@@ -64,9 +64,9 @@ function About () {
                     }
                 { openBefore ? <div className = "aboutbackground z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-sm md:text-md lg:text-lg 2xl:text-2xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center p-8 rounded-full ">
                     <div>
-                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-50">
+                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-70">
                         I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career partnering with multi-lingual, cross-functional teams to resolve conflicts. </p>
-                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-50">My unique professional background in complex problem-solving in person-focused roles allows me to better collaborate and communicate with diverse teams. Technology is built WITH people, FOR people.</p>
+                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-70">My unique professional background in complex problem-solving in person-focused roles allows me to better collaborate and communicate with diverse teams. Technology is built WITH people, FOR people.</p>
                         </div>
                     </div> 
                 
@@ -75,11 +75,11 @@ function About () {
                 </div>}
 
                 {openNow ? 
-                <div className = "aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-lg sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:p-8 rounded-full ">
+                <div className = "aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-lg sm:text-xl lg:text-2xl w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:p-8 rounded-full ">
                     <div className="">
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-50">I am currently learning Java, React Native, and Typescript.</p> 
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-50">I am collaborating on several projects with different teams to build quality, creative products.</p>
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-50"> In my free time, I'm probably kickboxing or thrifting somewhere.</p>
+                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I am currently learning Java, React Native, and Typescript.</p> 
+                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I am collaborating on several projects with different teams to build quality, creative products.</p>
+                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70"> In my free time, I'm probably kickboxing or thrifting somewhere.</p>
                     </div>
                 </div> 
                 
