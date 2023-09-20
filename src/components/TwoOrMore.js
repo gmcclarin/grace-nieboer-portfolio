7309513d2f3e4a133dd17ea9c5883606db40a2b0
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-20 12:19:58 -0400

    fix story and library pages

--- src/components/TwoOrMore.js
+++ src/components/TwoOrMore.js
@@ -63,17 +63,17 @@ export default function TwoOrMore () {
                 <div className="snap-start snap-always w-screen h-screen">
                         <div className="lg:flex w-screen h-screen pt-12 px-10 sm:px-32 justify-between items-center">
                             <div className="w-full lg:w-1/2">
-                                <div id="colorPalette" className="xl:text-8xl md:text-7xl text-4xl font-black lg:w-1/3 w-full">MISSION AND VISION</div>
-                                <div className=" w-full lg:w-1/3  lg:my-8 text-xl 2xl:text-3xl text-justify font-bold">Dive into the story behind Two or More.
+                                <div id="colorPalette" className="xl:text-8xl md:text-5xl text-4xl font-black lg:w-1/3 w-full">MISSION AND VISION</div>
+                                <div className=" w-full lg:my-8 text-xl 2xl:text-3xl font-bold">Dive into the story behind Two or More.
                                 </div>
-                                <div className="sm:pl-32 w-full lg:w-3/4 p-5 text-lg 2xl:text-2xl text-justify font-bold"> Two or More exist in their journey to serve bi/multi-ethnic individuals and their families. Get to know the founder and partners to learn more about the heart behind the non-profit.
+                                <div className="md:pl-32 pl-16  w-full lg:w-3/4 my-3 text-lg 2xl:text-2xl text-justify font-bold"> Two or More exist in their journey to serve bi/multi-ethnic individuals and their families. Get to know the founder and partners to learn more about the heart behind the non-profit.
                                 </div>
                             </div>
                             <div className=" w-full lg:w-1/2 flex-col sm:justify-center aspect-video">
                             <img className="w-1/2 lg:w-full p-2" src={why}  allow="autoplay"  alt="why" />
                             <div className="flex">
-                                <img className="w-1/3 lg:w-1/2 p-2" src={about}  allow="autoplay"  alt="about"  />
-                                <img className="w-1/3 lg:w-1/2 p-2" src={meet}  allow="autoplay"  alt="meet"  />
+                                <img className="w-1/3 md:w-1/4 lg:w-1/3 p-2" src={about}  allow="autoplay"  alt="about"  />
+                                <img className="w-1/3 md:w-1/4  lg:w-1/3 p-2" src={meet}  allow="autoplay"  alt="meet"  />
                             </div>
                             </div>
                         </div>
@@ -82,9 +82,9 @@ export default function TwoOrMore () {
                         <div className="lg:flex w-screen pt-12 px-10 sm:px-32 justify-between items-center">
                             <div>
                             <div className="xl:text-8xl md:text-7xl text-4xl font-black sm:w-1/3 w-full">LIBRARY</div>
-                            <div className=" w-full lg:w-1/3 my-8 text-xl 2xl:text-3xl text-justify font-bold">Resources for learning and growth  
+                            <div className=" w-full lg:w-1/3 my-4 sm:my-8 text-xl 2xl:text-3xl font-bold">Resources for learning and growth  
                             </div>
-                            <div className="sm:pl-32 w-full lg:w-3/4 p-5 text-lg 2xl:text-2xl text-justify font-bold">The LIBRARY provides helpful educational resources, categorized by various material types (digital, print, audio, etc). It also features a vocabulary page that will highlight rotating founder-chosen words that have important meaning within the community.
+                            <div className="sm:pl-32 pl-16 w-full lg:w-1/2 p-5 text-lg 2xl:text-2xl text-justify font-bold">The LIBRARY provides helpful educational resources, categorized by various material types (digital, print, audio, etc). It also features a vocabulary page that will highlight rotating founder-chosen words that have important meaning within the community.
                             </div>
                             </div>
                            
@@ -95,12 +95,12 @@ export default function TwoOrMore () {
                         <div className="lg:flex w-screen h-screen pt-12 px-10 sm:px-32 justify-between items-center">
                             <div className="w-full lg:w-1/2">
                                 <div id="colorPalette" className="xl:text-8xl md:text-7xl text-4xl font-black lg:w-1/3 w-full">STORIES</div>
-                                <div className=" w-full lg:w-1/3 my-8 text-xl 2xl:text-3xl text-justify font-bold">Everyone has a story.
+                                <div className=" w-full my-8 text-xl 2xl:text-3xl font-bold">Everyone has a story.
                                 </div>
                                 <div className="sm:pl-32 w-full lg:w-3/4 p-5 text-lg 2xl:text-2xl text-justify font-bold"> The Story page will feature reader submissions sharing their story. Everyone is welcome to browse stories and are encouraged to share their own experience.
                                 </div>
                             </div>
-                            <div className=" w-full lg:w-1/2 flex sm:justify-center aspect-video">
+                            <div className=" w-full md:w-1/2 flex sm:justify-center aspect-video">
                             <iframe className="w-full" src="https://player.vimeo.com/video/859198489?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479autoplay=1&loop=1&autopause=0?muted=1"  allow="autoplay"  title="share" />
                             </div>
                         </div>
