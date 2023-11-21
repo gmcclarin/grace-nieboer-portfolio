913f101f8271acc2bf0b41c3ed359d068d0e3622
Grace Nieboer <grace.nieboer@outlook.com>
2023-11-20 22:55:49 -0500

    finish home component

--- src/components/About.js
+++ src/components/About.js
@@ -48,11 +48,11 @@ function About () {
             
             <div className="relative sm:mx-20 lg:mx-32 mb-5 flex bg-neutral-100 h-screen items-center justify-center overscroll-x-auto overscroll-y-hidden">
 
-                {openAbout ? <div className = "aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl lg:text-2xl  w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:px-8 rounded-full ">
-                    <div>
-                        <p className="m-4 p-2 text-white bg-gray-900 opacity-70">HI, I'M GRACE! </p>
-                        <p className="m-4 p-2 text-white bg-gray-900 opacity-70"> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
-                       <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I love Python as a language, along with building API's with REST-ful architecture.
+                {openAbout ? <div className = "aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl lg:text-2xl  w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+                    <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
+                        <p className="m-6 p-5 text-white  ">HI, I'M GRACE! </p>
+                        <p className="m-4 p-2 text-white "> I am a full stack software engineer with a love for the creative elements of front-end design.</p>
+                       <p className="m-4 p-2 text-white ">I love Python as a language, along with building API's with REST-ful architecture.
                         </p>
                     </div>
                     </div> 
@@ -62,11 +62,11 @@ function About () {
                     </div> 
                     
                     }
-                { openBefore ? <div className = "aboutbackground z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-sm md:text-md lg:text-lg 2xl:text-2xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center p-8 rounded-full ">
-                    <div>
-                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-70">
+                { openBefore ? <div className = "aboutbackground z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-sm md:text-md lg:text-lg 2xl:text-2xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+                    <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
+                        <p className="my-1 sm:m-4 p-2 text-white rounded-lg">
                         I have a Master's degree in Public Administration, with a professional background in Human Resources. I've spent the past few years of my career partnering with multi-lingual, cross-functional teams to resolve conflicts. </p>
-                        <p className="my-1 sm:m-4 p-2 text-white bg-gray-900 opacity-70">My unique professional background in complex problem-solving in person-focused roles allows me to better collaborate and communicate with diverse teams. Technology is built WITH people, FOR people.</p>
+                        <p className="my-1 sm:m-4 p-2 text-white rounded-lg">My unique professional background in complex problem-solving in person-focused roles allows me to better collaborate and communicate with diverse teams. Technology is built WITH people, FOR people.</p>
                         </div>
                     </div> 
                 
@@ -75,15 +75,17 @@ function About () {
                 </div>}
 
                 {openNow ? 
-                <div className = "aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-lg sm:text-xl lg:text-2xl w-3/4 xl:w-1/3 h-full flex justify-center items-center sm:p-8 rounded-full ">
-                    <div className="">
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I am currently learning Java, React Native, and Typescript.</p> 
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70">I am collaborating on several projects with different teams to build quality, creative products.</p>
-                    <p className="m-4 p-2 text-white bg-gray-900 opacity-70"> In my free time, I'm probably kickboxing or thrifting somewhere.</p>
+                <div className = "aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-lg sm:text-xl lg:text-2xl w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+                    <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
+                    <p className="m-4 p-2 text-white ">I am currently learning Java, React Native, and Typescript.</p> 
+                    <p className="m-4 p-2 text-white ">I am collaborating on several projects with different teams to build quality, creative products.</p>
+                    <p className="m-4 p-2 text-white"> In my free time, I'm probably kickboxing or thrifting somewhere.</p>
                     </div>
                 </div> 
                 
                 :<div onClick={handleOpenNow} className="aboutnow cursor-pointer w-1/5  h-3/4 bg-neutral-600 rounded-full transform transition duration-500 hover:scale-125 overflow-hidden flex justify-center items-center ">
+                    
+                    
                     <p  className=" -rotate-90 font-black text-7xl text-white">NOW</p> 
                 </div>
                 
@@ -91,8 +93,10 @@ function About () {
 
             {openConnect ? 
                 
-                <div className = "aboutconnect z-10 text-white bg-neutral-800 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-2/3 xl:w-1/3 h-full flex justify-center items-center p-8 rounded-full ">
-                    <HomePageContact />
+                <div className = "aboutconnect z-10 text-white bg-neutral-800 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-2/3 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+                    <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
+                        <HomePageContact />
+                    </div>
                 </div> 
                 
                 
