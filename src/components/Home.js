91d998e367bd85865125ec9229a47b902ed96105
Grace Nieboer <grace.nieboer@outlook.com>
2023-11-21 08:57:06 -0500

    css flex, change the mobile responsiove layout on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ function Home () {
             <div className="home bg-cover bg-fixed h-screen w-screen overflow-auto p-5" >
                 <div className="sm:flex w-full justify-center">
                     {/* left half side of the screen */}
-                    <div className="w-1/2 h-full flex flex-col items-center justify-between">
+                    <div className="sm:w-1/2 h-full flex flex-col items-center justify-between">
                         <div className="w-full flex  m-5">
                             <div className="w-1/2  mx-2 text-xs font-bold">
                                 FULL STACK DEVELOPER WITH A LOVE FOR DESIGN AND USER IMPACT.
@@ -25,7 +25,7 @@ function Home () {
                                 BUILDING CREATIVE SOLUTIONS THAT MAKE THE WORLD BETTER.
                             </div>
                         </div>
-                        <div className="relative w-full flex justify-center items-center h-0 pb-1/2 text-sm sm:text-2xl lg:text-2xl xl:text-3xl p-5">
+                        <div className="relative w-full flex justify-center items-center h-0 sm:pb-1/2 text-sm sm:text-2xl lg:text-2xl xl:text-3xl p-5">
                             <div className="absolute top-12 left-1/3 sm:left-1/4 -translate-y-1/2 -translate-x-1/2 font-bold">
                                     PASSIONATE 
                                     <div className="scroller">
@@ -44,61 +44,42 @@ function Home () {
                     </div>
 
                     {/* right half of the home page */}
-                    <div className="sm:w-1/2 h-full flex flex-col sm:items-center items-end justify-between relative">
-                        <div className="w-full ">
+                    <div className="sm:w-1/2 w-full h-full flex  items-end relative">
+                        <div className="w-11/12 flex flex-col">
                             <div className="z-30 flex justify-center items-center">
                                 <NavLink className=" hover:transition-transform hover:scale-125 flex text-right font-nyght lg:text-7xl text-4xl " exact="true" to='/about'>
-                                <p className=" lg:text-[250px] text-7xl sm:text-9xl">a</p>
+                                <p className=" lg:text-[250px] text-9xl ">a</p>
                                 <p className="items-end flex">bout.</p>
                                 </NavLink>
                             </div>
-                            {/* <div className="w-full flex relative h-0 z-1 justify-center items-center">
-                                <div className="w-32 h-32 absolute z-2 rounded-full">
-                                    <video
-                                        id=""
-                                        autoPlay={true} 
-                                        loop={true}
-                                        controls={false} 
-                                        playsInline
-                                        muted
-                                        className="video-element w-full absolute rounded-full"
-                                        
-                                        >
-                                            <source
-                                            src="https://assets.mixkit.co/videos/preview/mixkit-digital-animation-of-screens-4192-large.mp4"
-                                            type="video/mp4"
-                                            />
-                                        Your browser does not support the video tag.
-                                    </video>
-                                </div>
-                            </div> */}
                             <div className="flex justify-center items-center">
                                 <NavLink className=" flex font-nyght lg:text-7xl text-4xl hover:transition-transform hover:scale-125" exact="true" to='/portfolio'>
-                                <p className="lg:text-[250px] sm:text-9xl text-7xl">w</p><p className="items-end flex">ork.</p></NavLink>
+                                <p className="lg:text-[250px] text-9xl ">w</p><p className="items-end flex">ork.</p></NavLink>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex items-end my-10 ">
-                    <div className="flex flex-col content-center items-center text-center border-orange-500 border-2 rounded-full py-1 overflow-y-hidden ">
-                        <div className="flex flex-col scroll-y">
-                            <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150 " href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon  icon={faLinkedin} style={{height:"20px",color: "#f97316"}}/>
-                            </a>
-                            <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="mailto:grace.nieboer@outlook.com">
-                                <FontAwesomeIcon icon={faEnvelope} style={{height:"20px", color:"#f97316", }}/>
-                            </a>
-                            <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://www.instagram.com/grayce.elisabeth_/">
-                                <FontAwesomeIcon icon={faInstagram} style={{height:"20px", color:"#f97316", }}/>
-                            </a>
-                            <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://medium.com/@grace.nieboer">
-                                <FontAwesomeIcon icon={faMedium} style={{height:"20px",color: "#f97316", }}/>
-                            </a>
-                            <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://github.com/gnieb">
-                                <FontAwesomeIcon icon={faGithub} style={{height:"20px",color: "#f97316",}} />
-                            </a>
+                    
+                        <div className="flex border-orange-500 border-2 rounded-full py-1 overflow-y-hidden ">
+                            <div className="flex flex-col scroll-y">
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150 " href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
+                                <FontAwesomeIcon  icon={faLinkedin} style={{height:"20px",color: "#f97316"}}/>
+                                </a>
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="mailto:grace.nieboer@outlook.com">
+                                    <FontAwesomeIcon icon={faEnvelope} style={{height:"20px", color:"#f97316", }}/>
+                                </a>
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://www.instagram.com/grayce.elisabeth_/">
+                                    <FontAwesomeIcon icon={faInstagram} style={{height:"20px", color:"#f97316", }}/>
+                                </a>
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://medium.com/@grace.nieboer">
+                                    <FontAwesomeIcon icon={faMedium} style={{height:"20px",color: "#f97316", }}/>
+                                </a>
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150" href="https://github.com/gnieb">
+                                    <FontAwesomeIcon icon={faGithub} style={{height:"20px",color: "#f97316",}} />
+                                </a>
                             </div>
                         </div>
+                    
                     </div>
+                    
                 </div>
             </div>
 
