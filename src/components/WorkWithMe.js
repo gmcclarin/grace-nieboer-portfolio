39ef29a3e7c8f1bf6a9869168c64c6597b51cb3f
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-28 12:29:18 -0500

    create a work with me component to be displayed on every page

---
+++ src/components/WorkWithMe.js
@@ -0,0 +1,13 @@
+
+
+export default WorkWithMe = () => {
+    return (
+        <div className="flex border-orange-600 border-2 rounded-full py-1 overflow-y-hidden overflow-x-hidden ">
+                            <div className="flex flex-col scroll-y text-orange-600">
+                                <a className="mx-1 sm:mx-2 transform transition duration-500 hover:scale-150 " href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
+                                LET'S WORK TOGETHER
+                                </a>
+                            </div>
+                        </div>
+    )
+}
