5c19a0310398dd6b6c78976b584aed56cc0e44bc
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-28 13:31:17 -0500

    change a tag in work with me to a navlink that pulls over to contact page

--- src/components/WorkWithMe.js
+++ src/components/WorkWithMe.js
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom"
 
 
 export const WorkWithMe = () => {
     return (
         <div className="z-40 flex border-orange-600 border-2 rounded-full py-1 overflow-y-hidden overflow-x-hidden ">
             <div className="flex flex-col scroll-x text-orange-600">
-                <a className="mx-1 sm:mx-2 transform transition duration-500  " href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
+                <NavLink 
+                className="mx-1 sm:mx-2 transform transition duration-500"
+                to="/contact"
+                exact="true">
                 LET'S WORK TOGETHER
-                </a>
+                </NavLink>
             </div>
         </div>
     )
