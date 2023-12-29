47440971bc7f96a9da280e404dd887273775dff3
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-29 15:53:15 -0500

    implement another verison of the dark mode in which a cookies are set to remember user preference for light and dark modes, with sun and moon icons to differentiate

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,28 @@ import MobileMenu from "./MobileMenu";
 import nightwind from "nightwind/helper";
 import circle from "../photos/enormouscirclegradient.png"
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 import { WorkWithMe } from "./WorkWithMe";
+// import Cookies from js-cookie
+import Cookies from 'js-cookie';
 
 function Home () {
 
-    const [darkMode, setDarkMode] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
     
+    
+    
+    useEffect(() => {
+        if (Cookies.get('darkMode') == 'true') {
+            setDarkMode((darkMode) => true)
+        } else {
+            setDarkMode((darkMode) => false)
+        }
+        
+    }, [])
+
+    console.log("darkMode state:", darkMode)
+
 
     return (
 
@@ -25,18 +41,55 @@ function Home () {
                 {/* toggle dark mode */}
                 <div className="z-40 flex items-center justify-between w-full sm:mb-12 sm:mt-5 mt-7 mb-7">
                     <WorkWithMe />
-                    <label for="toggleB" className="flex items-center cursor-pointer">
-                        <div className="relative">
-                            <input type="checkbox" id="toggleB" className="sr-only bg-neutral-300" onClick={() => nightwind.toggle()} />
-                            <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                    
+                        {/* <div className="relative">
+                            <input 
+                            type="checkbox" 
+                            id="toggleB" 
+                            checked={darkMode} 
+                            className="sr-only bg-neutral-300" 
+                            onChange={() => {
+                                Cookies.set('darkMode', !darkMode)
+                                setDarkMode(darkMode)
+                                nightwind.toggle()
+
+                                
+                            }}
+                            // onClick={ () =>  {
+                                
+                            //     }}
+                                 /> */}
+                            {/* <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
                             <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition">
                                 <div className="text-white m-auto">
                                     <FontAwesomeIcon  icon={faMoon} style={{height:"20px", margin:"auto", padding:"2px"}}/>
                                 </div>
                             </div>
                         </div>
-                        {/* <div className="ml-3 text-gray-700 font-medium">Toggle Me!</div> */}
-                    </label>
+                       
+                     */}
+                     <div className="relative">
+                        <div 
+                            className="flex justify-center items-center rounded-full bg-black w-10 h-10 cursor-pointer"
+                            onClick={() => {
+                                setDarkMode(!darkMode)
+                                nightwind.toggle()
+                                Cookies.set('darkMode', !darkMode)
+                            }}
+
+                        >
+                            {darkMode? 
+                            <div className="text-yellow-600 m-auto ">
+                                <svg className="fill-yellow-600" xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
+                            </div> :
+                            <div className="text-yellow-600 m-auto">
+                                <svg className="fill-yellow-600" xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                            </div> }
+                            {/* <div 
+                            className={darkMode ? "absolute left-1 top-1 rounded-full bg-gray-100 w-10 h-10 cursor-pointer" : "rounded-full bg-black w-10 h-10 cursor-pointer left-1 top-1"}
+                            ></div> */}
+                        </div>
+                    </div>
                 </div>
 
                 <div className="sm:flex w-full justify-center">
