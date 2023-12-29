deac86d9586e2bda85763300a45665c23970cddd
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-29 15:56:53 -0500

    improve visibility for the top marquis in darkm mode with indigo. working on custom nightwing config colors

--- src/components/WorkWithMe.js
+++ src/components/WorkWithMe.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom"
 
 export const WorkWithMe = () => {
     return (
-        <div className="z-40 flex border-orange-600 border-2 rounded-full py-1 overflow-y-hidden overflow-x-hidden ">
-            <div className="flex flex-col scroll-x text-orange-600">
+        <div className="z-40 flex border-indigo-950 border-2 rounded-full py-1 overflow-y-hidden overflow-x-hidden ">
+            <div className="flex flex-col scroll-x text-indigo-950">
                 <NavLink 
                 className="mx-1 sm:mx-2 transform transition duration-500"
                 to="/contact"
