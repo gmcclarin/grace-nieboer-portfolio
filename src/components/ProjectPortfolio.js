46534cf591ba168125993942c99b05f62c5932e6
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-11 22:26:38 -0400

    clean up the portfolio

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -19,23 +19,17 @@ function ProjectPortfolio () {
                     <div className="snap-y snap-mandatory h-screen w-screen overflow-x-hidden text-3xl md:text-4xl lg:text-6xl xl:text-8xl">
                         <div id="" className=" bg-white w-screen h-5/6 flex items-center justify-center ">
                             <RevealOnScroll>
-                            
                                 <div className="sm:ml-24 lg:ml-0 w-3/4 ">
-                                        
-                                            <div className="p-5">
-                                                <div className="font-bold text-2xl text-left">Current project, in progress</div>
-                                                <div className="text-xl text-left italic">deployment estimated for march 2024 </div>
-                                                
-                                            </div>
-                                            <div className="text-left text-xl p-5">Tylers Tree Solutions</div>
-                                        
-                                    
+                                    <div className="p-5">
+                                        <div className="font-bold text-2xl text-left">Current project, in progress</div>
+                                        <div className="text-xl text-left italic">deployment estimated for march 2024 </div>
+                                    </div>
+                                    <div className="text-left text-xl p-5">Tylers Tree Solutions</div>
                                 </div>
 
                                 <div className="w-full h-full flex justify-end text-right">
                                     <div className="w-1/2  "><img src="https://i.postimg.cc/5NCbQ9HY/Colorful-Noise-Gradient-Motivational-Quote-Instagram-Post-1.png"/></div>
                                 </div>
-                            
                             </RevealOnScroll>
                         </div>
 
