b7f72ad1042a90b79b239dc22c16b9876135b556
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-18 17:24:08 -0400

    the cutest scroll animation

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -42,6 +42,16 @@ function ProjectPortfolio () {
 
                             </div>
                             </RevealOnScroll>
+                            <div
+                                 className="flex fixed right-1/2 bottom-8"
+                                >
+                                    <p className="text-lg lg:text-xl m-1 mb-4 font-bold text-orange-700 font-nyght">scroll</p>
+                                    <svg
+                                    xmlns="http://www.w3.org/2000/svg" height="30" width="30"
+                                    className="animate-bounce fill-orange-700" viewBox="0 0 384 512">
+                                        <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
+                                    </svg>
+                                </div>
                         </div>
 
 
