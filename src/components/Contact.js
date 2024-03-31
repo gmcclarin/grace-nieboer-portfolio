579802f9f2f1f3deca8e0b4f78c9d032bd110225
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-31 19:06:33 -0400

    add transformationanimation on social media for conect page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,19 +24,22 @@ export default function Contact () {
             
             <div className="z-30 p-5 content-center text-center">
                 <h1  className="text-5xl sm:text-7xl md:text-[80px] lg:text-[100px] xl:text-[150px] font-nyght font-bold" >connect with me.</h1>
-                <a href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
+                <a className="hover:scale-150"
+                href="https://www.linkedin.com/in/gracenieboer/" target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} style={{height:"40px",color: "#262626", margin:"10px"}}/>
                 </a>
-                <a href="mailto:grace.nieboer@outlook.com">
+                <a className="hover:scale-150"
+                href="mailto:grace.nieboer@outlook.com">
                     <FontAwesomeIcon icon={faEnvelope} style={{height:"40px", color:"#262626", margin:"9px"}}/>
                 </a>
-                <a href="https://www.instagram.com/grayce.elisabeth_/">
+                <a className="hover:scale-150"
+                href="https://www.instagram.com/grayce.elisabeth_/">
                     <FontAwesomeIcon icon={faInstagram} style={{height:"40px", color:"#262626", margin:"9px"}}/>
                 </a>
-                <a className="mx-2" href="https://medium.com/@grace.nieboer">
+                <a className="mx-2 hover:scale-150" href="https://medium.com/@grace.nieboer">
                     <FontAwesomeIcon icon={faMedium} style={{height:"40px",color: "#262626", margin:"8px"}}/>
                 </a>
-                <a className="mx-2" href="https://github.com/gnieb">
+                <a className="mx-2 hover:scale-150" href="https://github.com/gnieb">
                     <FontAwesomeIcon icon={faGithub} style={{height:"40px",color: "#262626", margin:"8px"}} />
                 </a>
             </div>
@@ -50,12 +53,6 @@ export default function Contact () {
                     </div>
                 </div>
                 <div className="z-40 absolute bottom-28 right-10 flex justify-end w-full ">
-                    {/* <div className="z-40 flex justify-center border-indigo-800 border-2 rounded-full py-3 w-1/4 sm:w-1/5">
-                        <div 
-                            className="text-indigo-800 text-lg font-black px-3 cursor-pointer"
-            
-                            >CERTIFICATION</div>
-                    </div> */}
 
                     <CertModal showModal={showModal} setShowModal={setShowModal} />
                 </div>
