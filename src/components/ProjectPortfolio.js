fd126dbc4c9e91a311493ddf7332452f9971947d
Grace Nieboer <grace.nieboer@outlook.com>
2025-05-21 23:38:05 -0400

    remove the tacky scroll thing

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -55,20 +55,6 @@ function ProjectPortfolio() {
                   </div>
                 </div>
               </RevealOnScroll>
-              <div className="flex fixed right-1/2 bottom-10">
-                <p className="text-lg lg:text-xl m-1 mb-4 font-bold text-orange-600 font-nyght">
-                  scroll
-                </p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="30"
-                  width="30"
-                  className="animate-bounce fill-orange-600"
-                  viewBox="0 0 384 512"
-                >
-                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                </svg>
-              </div>
             </div>
             <div className="h-full  grid grid-flow-row-dense grid-cols-3 gap-4 m-5">
               <NavLink
