32a27251d656c9e22424c6ca04661431bdd9fcc3
Grace Nieboer <grace.nieboer@outlook.com>
2025-05-22 23:51:05 -0400

    new photo for two or more, layout under construction

--- src/components/ProjectPortfolio.js
+++ src/components/ProjectPortfolio.js
@@ -7,7 +7,7 @@ import DarkMode from "./DarkMode";
 
 function ProjectPortfolio() {
   return (
-    <div className="bg-white relative block no-scrollbar overflow-y-scroll overflow-x-hidden">
+    <div className="bg-white relative block no-scrollbar overflow-x-hidden">
       <div className="sm:hidden ">
         <MobileMenu />
       </div>
@@ -17,12 +17,12 @@ function ProjectPortfolio() {
       <NavBar />
       <div className="flex">
         <div className="mx-auto text-center">
-          <div className="h-screen w-screen text-3xl md:text-4xl lg:text-6xl xl:text-7xl">
+          <div className="w-screen text-3xl md:text-4xl lg:text-6xl xl:text-7xl">
             <div
-              className=" bg-white w-screen h-5/6 mb-16 flex items-center justify-center "
+              className=" bg-white w-screen mb-16 flex items-center justify-center "
             >
               <RevealOnScroll>
-                <div className="w-full h-full">
+                <div className="w-full">
                   <div className=" w-full sm:pl-24 py-10 px-5 ">
                     <div className="p-5 text-black">
                       <div className="font-bold text-2xl text-left animate-pulse">
@@ -44,10 +44,10 @@ function ProjectPortfolio() {
                       </div>
                     </div>
                   </div>
-                  <div className="w-full h-1/2 flex justify-center sm:justify-end text-right">
-                    <div className="w-3/4 sm:w-1/2 h-full">
+                  <div className="w-full flex justify-center sm:justify-end text-right">
+                    <div className="w-3/4 sm:w-1/2">
                       <img
-                        className="object-contain w-full h-full"
+                        className="object-contain w-full"
                         src="https://i.postimg.cc/9QNp5c9g/blurryholdinghands.avif"
                         alt="holding_hands"
                       />
@@ -56,24 +56,20 @@ function ProjectPortfolio() {
                 </div>
               </RevealOnScroll>
             </div>
-            <div className="h-full  grid grid-flow-row-dense grid-cols-3 gap-4 m-5">
+            <div className=" grid grid-flow-row-dense grid-cols-3 gap-7 m-8 ">
               <NavLink
                 id="twofirstsection"
-                className=" w-full h-full row-span-4 col-span-2 bg-pink-800 transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl"
+                className=" w-full row-span-4 col-span-2 bg-pink-800 transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl"
                 to="/twoormore"
                 exact="true"
               >
               </NavLink>
-
-              
-                
                 <NavLink to="/jobsurge" exact="true" className="bg-stone-600 row-span-2 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl">
                   <Time />
                 </NavLink>
 
                 <NavLink id="portsection1" to="/inspiredinteriors" exact="true"
                 className="bg-stone-600 flex flex-col row-span-2 items-center justify-center transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl">
-                  INSPIRED INTERIORS
                 </NavLink>
             
             </div>
