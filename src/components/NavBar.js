c9c1486e2b220690cdef73e5c30aecfc28c1b772
Grace Nieboer <grace.nieboer@outlook.com>
2025-06-22 21:55:07 -0400

    clean up nav bar and reposition

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,31 @@
-
 import { NavLink } from "react-router-dom";
 
-function NavBar () {
-    return (
-        <nav className="z-50 text-neutral-800 ml-7 my-7 hidden sm:flex sm:flex-col h-100vh bg-white rounded-full content-center justify-center sm:fixed">
-                            <NavLink
-                            to="/"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md py-20  text-sm sm:text-xl font-bold -rotate-90"
-                            >
-                                HOME
-                            </NavLink>
-                            <NavLink
-                            to="/about"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md py-20 text-sm sm:text-xl font-bold -rotate-90"
-                            >
-                                ABOUT
-                            </NavLink>
-                            <NavLink
-                            to="/portfolio"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md py-20 text-sm sm:text-xl font-bold -rotate-90"
-                            >
-                                WORK
-                            </NavLink>
-                            {/* <NavLink
-                            to="/contact"
-                            exact="true"
-                            className="hover:text-orange-500 rounded-md py-20 text-sm sm:text-xl font-bold -rotate-90"
-                            >CONTACT
-                            </NavLink> */}
-        </nav>
-    )
+function NavBar() {
+  return (
+    <nav className="z-50 text-neutral-800 ml-7 my-20 hidden sm:flex sm:flex-col h-100vh bg-white rounded-full content-center justify-center sm:fixed">
+      <NavLink
+        to="/"
+        exact="true"
+        className="hover:text-orange-500 rounded-md py-20  text-sm sm:text-xl font-bold -rotate-90"
+      >
+        HOME
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact="true"
+        className="hover:text-orange-500 rounded-md py-20 text-sm sm:text-xl font-bold -rotate-90"
+      >
+        ABOUT
+      </NavLink>
+      <NavLink
+        to="/portfolio"
+        exact="true"
+        className="hover:text-orange-500 rounded-md py-20 text-sm sm:text-xl font-bold -rotate-90"
+      >
+        WORK
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;
