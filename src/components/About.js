4d833bdd01abe513461bd2892bb217b8dd128580
Grace Nieboer <grace.nieboer@outlook.com>
2025-08-07 21:21:48 -0400

    about styling

--- src/components/About.js
+++ src/components/About.js
@@ -51,23 +51,22 @@ function About() {
 
       <div className="relative sm:mx-20 lg:mx-32 mb-5 flex  h-screen items-center justify-center overscroll-x-auto overscroll-y-hidden">
         {openAbout ? (
-          <div className="aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl lg:text-2xl  w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+          <div className="aboutabout  z-10 text-white font-bold  top-50 left-50 text-xl lg:text-2xl  w-3/4 xl:w-1/4 h-3/4 flex justify-center items-center rounded-full ">
             <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
-              <p className="m-6 p-5 text-white  ">Hi, I'm Grace! </p>
               <p className="m-4 p-2 text-white ">
-                {" "}
-                I am a full stack software engineer with a love for the creative
-                elements of UX / UI
-              </p>
-              <p className="m-4 p-2 text-white ">
-                In my free time, I'm probably kickboxing or thrifting somewhere.
+                Hi, I’m Grace — a full-stack software engineer passionate about
+                clean, human-centered design. I bring creativity into every
+                corner of my work, whether it's crafting elegant code or
+                intuitive user flows. When I'm not building, you’ll probably
+                find me at kickboxing or hunting for vintage gems in thrift
+                stores.
               </p>
             </div>
           </div>
         ) : (
           <div
             onClick={handleOpenAbout}
-            className="aboutabout cursor-pointer w-1/5 h-3/4   bg-neutral-400 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden "
+            className="aboutabout cursor-pointer w-1/5 h-1/2   bg-neutral-400 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden "
           >
             <div className="flex items-end -rotate-90 font-black text-4xl sm:text-7xl text-orange-600 font-nyght ">
               <p>hi</p>
@@ -75,26 +74,21 @@ function About() {
           </div>
         )}
         {openBefore ? (
-          <div className="aboutbackground z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-sm md:text-md lg:text-lg 2xl:text-2xl text-justify w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+          <div className="aboutbackground z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-sm md:text-md lg:text-lg 2xl:text-2xl w-3/4 xl:w-1/4 h-3/4 flex justify-center items-center rounded-full ">
             <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
               <p className="my-1 sm:m-4 p-2 text-white rounded-lg">
-                I have a Master's degree in Public Administration, with a
-                professional background in Human Resources. I've spent the past
-                few years of my career partnering with multi-lingual,
-                cross-functional teams to resolve conflicts.{" "}
-              </p>
-              <p className="my-1 sm:m-4 p-2 text-white rounded-lg">
-                My unique professional background in complex problem-solving in
-                person-focused roles allows me to better collaborate and
-                communicate with diverse teams. Technology is built WITH people,
-                FOR people.
+                Before tech, I earned my Master’s in Public Administration and
+                worked in Human Resources. I specialized in conflict resolution
+                and supported multilingual, cross-functional teams. That
+                experience taught me the power of empathy and communication —
+                two tools I now use daily in collaborative software development.
               </p>
             </div>
           </div>
         ) : (
           <div
             onClick={handleOpenBefore}
-            className="aboutbackground cursor-pointer w-1/5 h-3/4 bg-neutral-400 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden "
+            className="aboutbackground cursor-pointer w-1/5 h-1/2 bg-neutral-400 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden "
           >
             <div className="flex items-end -rotate-90 font-black text-4xl sm:text-7xl text-orange-600 font-nyght ">
               <p>background</p>
@@ -103,23 +97,17 @@ function About() {
         )}
 
         {openNow ? (
-          <div className="aboutnow z-10 text-white font-bold bg-neutral-600 top-50 left-50 text-lg sm:text-xl lg:text-2xl w-3/4 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+          <div className="aboutnow z-10 text-white bg-neutral-600 top-50 left-50 text-lg sm:text-xl lg:text-2xl w-3/4 xl:w-1/4 h-3/4 flex justify-center items-center rounded-full ">
             <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
               <p className="m-4 p-2 text-white ">
-                I work @ Wilmac Technologies with several cross-functional teams
-                to enhance their Continuity Replay product.
-              </p>
-              <p className="m-4 p-2 text-white ">
-                I am currently learning C# and various AWS services; I recently
-                received the AWS Cloud Practitioner certification and plan to
-                continue learning!
+                I’m currently a software engineer at Wilmac Technologies, enhancing their flagship product, Continuity Replay. I work closely with product, support, and engineering teams across the stack. I recently earned my AWS Cloud Practitioner certification and am diving deeper into C# and cloud architecture.
               </p>
             </div>
           </div>
         ) : (
           <div
             onClick={handleOpenNow}
-            className="aboutnow cursor-pointer w-1/5  h-3/4 bg-neutral-600 rounded-full transform transition duration-500 hover:scale-125 overflow-hidden flex justify-center items-center "
+            className="aboutnow cursor-pointer w-1/5  h-1/2 bg-neutral-600 rounded-full transform transition duration-500 hover:scale-125 overflow-hidden flex justify-center items-center "
           >
             <div className="flex items-end -rotate-90 font-black text-4xl sm:text-7xl text-orange-600 font-nyght ">
               <p>now</p>
@@ -128,15 +116,16 @@ function About() {
         )}
 
         {openConnect ? (
-          <div className="aboutconnect z-10 text-white bg-neutral-800 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-2/3 xl:w-1/3 h-full flex justify-center items-center rounded-full ">
+          <div className="aboutconnect z-10 text-white bg-neutral-800 top-50 left-50 text-xl sm:text-2xl lg:text-3xl xl:text-3xl text-justify w-2/3 xl:w-1/4 h-3/4 flex justify-center items-center rounded-full ">
             <div className="flex flex-col items-center justify-center bg-gray-900 opacity-70 rounded-full h-full w-full">
+              <p className="m-4 p-2 text-white">Let’s build something great — or just say hey! Whether it’s a project idea, a tech question, or a meme worth sharing, I’d love to hear from you.</p>
               <HomePageContact />
             </div>
           </div>
         ) : (
           <div
             onClick={handleOpenConnect}
-            className="aboutconnect cursor-pointer w-1/5 h-3/4 bg-neutral-800 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden"
+            className="aboutconnect cursor-pointer w-1/5 h-1/2 bg-neutral-800 rounded-full transform transition duration-500 hover:scale-125 flex justify-center items-center overflow-hidden"
           >
             <div className="flex items-end -rotate-90 font-black text-4xl sm:text-7xl text-orange-600 font-nyght ">
               <p>connect</p>
