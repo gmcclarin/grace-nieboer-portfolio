02f30eff4aae707f1ef9f465a531911f60969c3f
Grace Nieboer <grace.nieboer@outlook.com>
2025-08-08 22:43:22 -0400

    portfolio layout v2

--- src/components/ProjectPortfolioV2.js
+++ src/components/ProjectPortfolioV2.js
@@ -1,10 +1,17 @@
+import MobileMenu from "./MobileMenu";
+import NavBar from "./NavBar";
+
 export default function ProjectPortfolio2() {
   return (
     <div className="bg-black h-screen w-screen">
-    <div class="grid h-full w-full px-20 grid-flow-col grid-rows-3 gap-5">
+      <div className="sm:hidden">
+        <MobileMenu />
+      </div>
+      <NavBar />
+    <div class="grid h-full w-full px-32 grid-flow-col grid-rows-3 gap-7">
       {/* Column 1 */}
       <div class="row-span-3 bg-white">
-        
+        01
       </div>
       {/* Column 2 */}
       <div class="row-span-3 bg-white">02</div>
