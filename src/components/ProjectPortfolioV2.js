f0e1fad9e0dc48d82d68009f935d7f4de674e4d8
Grace Nieboer <grace.nieboer@outlook.com>
2025-08-14 22:44:57 -0400

    column 1

--- src/components/ProjectPortfolioV2.js
+++ src/components/ProjectPortfolioV2.js
@@ -2,29 +2,25 @@ import MobileMenu from "./MobileMenu";
 import NavBar from "./NavBar";
 import { NavLink } from "react-router-dom";
 import Time from "./Time";
-import { useEffect } from "react";
 import trees from "../photos/trees.png";
 import twoOrMore from "../photos/tomhome.png";
 import inspired from "../photos/photo20.avif";
-import lapTop from "../photos/roseLaptop.jpg";
-import circle from "../photos/enormouscirclegradient.png";
 import preview from "../photos/githubgenerator.png";
 
 export default function ProjectPortfolio2() {
+  const moreButtonStyle = "rounded-full bg-white px-6 py-2 flex justify-center"
+  
   return (
-    <div className="bg-zinc-100 h-screen w-screen overflow-hidden flex flex-col">
+    <div className="bg-zinc-100 h-screen w-screen overflow-hidden flex flex-col text-center">
       <div className="sm:hidden">
         <MobileMenu />
       </div>
       <NavBar />
       <div className="flex-1 min-h-0 ">
         <div className="grid grid-cols-1 lg:grid-cols-3 h-full w-full gap-8 px-40">
-          
           {/* Column 1 */}
           <div className="bg-white flex flex-col min-h-0">
-            <div className="mt-12 p-4 border-b text-black font-semibold">01</div>
-
-            <div className="flex-1 min-h-0 overflow-y-auto">
+            <div className="flex-1 min-h-0 overflow-y-auto mt-12">
               <div className="py-6 space-y-4">
                 <div className="p-6 text-center font-bold text-xl">
                   INSPIRED INTERIORS
@@ -33,11 +29,21 @@ export default function ProjectPortfolio2() {
                   <img src={inspired} alt="mini_inspired"/>
                 </div>
                 <div className="p-6 space-y-4 text-center">
-Bringing color, texture, and inspiration together — one moodboard at a time                </div>
-                <Time />
-                <div className="p-6 space-y-4">
+                  Bringing color, texture, and inspiration together — one moodboard at a time               
+                </div>
+                {/* JOB SURGE */}
+                  <div className="bg-stone-400 p-6 py-16">
+                  <Time />
+                  <div className="p-6 space-y-4 text-center">
+                    <div className="font-bold text-xl">JOB SURGE</div>
                   web scraping working hard here
                 </div>
+                {/* <div className="flex justify-stretch"> */}
+                  <NavLink to="/jobsurge" exact="true" className={moreButtonStyle}>Learn More</NavLink>
+                {/* </div> */}
+                </div>
+                
+                
               </div>
             </div>
           </div>
