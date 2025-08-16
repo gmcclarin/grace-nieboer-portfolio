c3eca1323358149540deb2fc17ae86e0a0e58d1e
Grace Nieboer <grace.nieboer@outlook.com>
2025-08-15 23:23:39 -0400

    color palette for light and dark

--- src/components/ProjectPortfolioV2.js
+++ src/components/ProjectPortfolioV2.js
@@ -8,10 +8,12 @@ import inspired from "../photos/photo20.avif";
 import preview from "../photos/githubgenerator.png";
 
 export default function ProjectPortfolio2() {
-  const moreButtonStyle = "rounded-full bg-white px-6 py-2 flex justify-center"
-  
+  const containedButtonStyle =
+    "rounded-full bg-white px-6 py-3 flex justify-center text-sm text-black";
+  const outlinedButtonStyle =
+    "rounded-full border-2 border-black px-6 py-3 flex justify-center text-sm text-black";
   return (
-    <div className="bg-zinc-100 h-screen w-screen overflow-hidden flex flex-col text-center">
+    <div className="bg-white h-screen w-screen overflow-hidden flex flex-col text-center">
       <div className="sm:hidden">
         <MobileMenu />
       </div>
@@ -19,65 +21,72 @@ export default function ProjectPortfolio2() {
       <div className="flex-1 min-h-0 ">
         <div className="grid grid-cols-1 lg:grid-cols-3 h-full w-full gap-8 px-40">
           {/* Column 1 */}
-          <div className="bg-white flex flex-col min-h-0">
+          <div className="bg-stone-100 flex flex-col min-h-0">
             <div className="flex-1 min-h-0 overflow-y-auto mt-12">
               <div className="py-6 space-y-4">
-                <div className="p-6 text-center font-bold text-xl">
-                  INSPIRED INTERIORS
+                <div className=" p-6 py-16">
+                  <div className="p-6 text-center font-bold text-xl text-black">
+                    INSPIRED INTERIORS
+                  </div>
+                  <div>
+                    <img src={inspired} alt="mini_inspired" />
+                  </div>
+                  <div className="p-6 space-y-4 text-cennter text-black">
+                    Bringing color, texture, and inspiration together — one
+                    moodboard at a time
+                  </div>
+                  <NavLink
+                    to="/inspiredinteriors"
+                    exact="true"
+                    className={outlinedButtonStyle}
+                  >
+                    Learn More
+                  </NavLink>
                 </div>
-                <div>
-                  <img src={inspired} alt="mini_inspired"/>
-                </div>
-                <div className="p-6 space-y-4 text-center">
-                  Bringing color, texture, and inspiration together — one moodboard at a time               
-                </div>
+
                 {/* JOB SURGE */}
-                  <div className="bg-stone-400 p-6 py-16">
+                <div className="bg-stone-400 p-6 py-16">
                   <Time />
                   <div className="p-6 space-y-4 text-center">
                     <div className="font-bold text-xl">JOB SURGE</div>
-                  web scraping working hard here
+                    web scraping working hard here
+                  </div>
+                  {/* <div className="flex justify-stretch"> */}
+                  <NavLink
+                    to="/jobsurge"
+                    exact="true"
+                    className={containedButtonStyle}
+                  >
+                    Learn More
+                  </NavLink>
+                  {/* </div> */}
                 </div>
-                {/* <div className="flex justify-stretch"> */}
-                  <NavLink to="/jobsurge" exact="true" className={moreButtonStyle}>Learn More</NavLink>
-                {/* </div> */}
-                </div>
-                
-                
               </div>
             </div>
           </div>
 
           {/* Column 2 */}
-          <div className="bg-white flex flex-col min-h-0 mt-24">
-            <div className="p-4 border-b font-semibold text-black">02</div>
+          <div className="bg-stone-200 flex flex-col min-h-0 mt-24">
             <div className="flex-1 min-h-0 overflow-y-auto">
-              <div className="p-6 space-y-4">
-                
-              </div>
+              <div className="p-6 space-y-4"></div>
             </div>
           </div>
 
           {/* Column 3 */}
-          <div className="bg-white flex flex-col min-h-0">
-            <div className="mt-12 p-4 border-b font-semibold text-black">03</div>
+          <div className="bg-stone-400 flex flex-col min-h-0 pt-24">
             <div className="flex-1 min-h-0 overflow-y-auto">
               <div className="p-6 space-y-4">
-                Currently working on the production of two new products in the Continuity Suite @ WILMAC Technologies :
-                 
+                Currently working on the production of two new products in the
+                Continuity Suite @ WILMAC Technologies :
               </div>
-              <div className="p-6 space-y-4">
-                Continuity Replay
-              </div>
+              <div className="p-6 space-y-4">Continuity Replay</div>
               <div className="p-6 space-y-4">
                 Continuity Workforce Management
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   );
 }
-
