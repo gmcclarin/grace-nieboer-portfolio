f0b7e730e6435c1990061cf641633905d5da52a8
Grace Nieboer <grace.nieboer@outlook.com>
2025-08-21 20:22:22 -0400

    fix error 404 page and navigation back

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,13 +4,14 @@ import NavBar from "./NavBar";
 
 export default function NotFound () {
     const navigate = useNavigate();
-    const goBackOne = () => navigate.goBack()
+    const goBackOne = () => navigate(-1)
 
 
     return (
-        <div className="bg-cover h-screen">
+        <div className="bg-cover h-screen w-screen">
             <NavBar />
-            <div className=" mt-20 p-5 text-5xl font-display font-bold text-center">
+            <div className="h-full w-full flex flex-col justify-center items-center">
+                 <div className=" mt-20 p-5 text-5xl font-display font-bold text-center">
                 Whoops! Page Not Found. 
             </div>
             <div className=" p-5 text-5xl font-display font-bold text-center">
@@ -21,6 +22,8 @@ export default function NotFound () {
                 onClick={goBackOne}>
                     GO BACK
                 </button>
+            </div>
+           
             </div>
         </div>
     )
